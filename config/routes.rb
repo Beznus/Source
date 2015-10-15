a6f332b22a2b7736201e5436dd80533a5354e618
Beznus <cassidy.1113@gmail.com>
2015-10-15 08:49:23 -0400

    front page some styling etc

--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,13 @@ Rails.application.routes.draw do
   resources :products
 
   devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions"}
-
   resources :users do
     member do
       get :dashboard
     end
   end
-  # The priority is based upon order of creation: first created -> highest priority.
+
+ # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
